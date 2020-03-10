cc5fb12002d1194d87f17f34743bfaffbcf0e576
Benedict Waiharo <benabana@live.com>
2020-03-10 08:56:19 -0400

    Trial 1

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ from config import POSTGRES_USER,POSTGRES_PW,POSTGRES_URL,POSTGRES_DB
 
 app = Flask(__name__)
 
-DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
-# app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or DB_URL
+# DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
 app.secret_key = 'secret'
 
@@ -30,9 +30,9 @@ Base.prepare(db.engine, reflect=True)
 Restate_table = Base.classes.comprehensive
 Individual = Base.classes.individual
 
-engine = create_engine(DB_URL)
-insp = reflection.Inspector.from_engine(engine)
-print(insp.get_table_names())
+# engine = create_engine(DB_URL)
+# insp = reflection.Inspector.from_engine(engine)
+# print(insp.get_table_names())
 
 @app.route("/")
 def index():
