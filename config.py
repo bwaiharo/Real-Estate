3d5bbccd356f6ea3a1b7a5a43a340623250ad84e
Benedict Waiharo <benabana@live.com>
2020-03-10 10:29:24 -0400

    No credentials

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 
 
-POSTGRES_URL="ec2-3-230-106-126.compute-1.amazonaws.com"
-POSTGRES_USER="aoornhhuimalta"
+POSTGRES_URL="****"
+POSTGRES_USER="****"
 
-POSTGRES_PW="2f178d20a48726ca43e7360f4d5dbfc639bb1dde1440d3bd8151102666fe5e5c"
-POSTGRES_DB="d6r0rupc24v4up"
+POSTGRES_PW="****"
+POSTGRES_DB="****"
 
 
 
